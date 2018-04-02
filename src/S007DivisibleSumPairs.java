cfe3bec76d34695acbda495263e5d04e08b9acba
J. Martin Amaris <jamaris@softclear.net>
2018-04-02 09:20:23 -0400

    Divisible Sum Pairs

--- src/S007DivisibleSumPairs.java
+++ src/S007DivisibleSumPairs.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class S007DivisibleSumPairs {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        // Complete this function
+        int divisibles = 0;
+        for (int i = 0; i < n; i++) {
+
+            int pivot = ar[i];
+
+            for (int j = i + 1; j < n; j++) {
+                if (((pivot + ar[j]) % k) == 0) {
+                    divisibles++;
+                }
+            }
+        }
+
+        return divisibles;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = divisibleSumPairs(n, k, ar);
+        System.out.println(result);
+    }
 }
