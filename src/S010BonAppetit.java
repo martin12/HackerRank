a3e8cb782f710a13cdd383ec7093fba4c3a4dedf
J. Martin Amaris <jamaris@softclear.net>
2018-04-02 09:20:50 -0400

    Bon Appetit

--- src/S010BonAppetit.java
+++ src/S010BonAppetit.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class S010BonAppetit {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int[] itemsOrdered = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            itemsOrdered[ar_i] = in.nextInt();
+        }
+
+        int charged = in.nextInt();
+
+        int inCommon = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != k) {
+                inCommon += itemsOrdered[i];
+            }
+        }
+
+        int sharedCharge = inCommon / 2;
+        if ( sharedCharge == charged) {
+            System.out.println("Bon Appetit");
+        }
+        else {
+            System.out.println(charged - sharedCharge);
+        }
+    }
 }
